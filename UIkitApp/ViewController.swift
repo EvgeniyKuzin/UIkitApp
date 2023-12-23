be1e71851f25790cfaf4e3efc9766fa5abdf572a
EvgeniyKuzin <vaglover@yandex.ru>
2023-12-23 22:39:43 +0300

    app is ready

--- UIkitApp/ViewController.swift
+++ UIkitApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var showGreeteingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
     }
 
-
+    
+    @IBAction func greetingButtonTapped() {
+        greetingLabel.isHidden.toggle()
+        if !greetingLabel.isHidden {
+            showGreeteingButton.setTitle("спрятать", for: .normal)
+        } else {
+            showGreeteingButton.setTitle("показать еще раз", for: .normal)
+        }
+    }
 }
 
