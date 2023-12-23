2942363933daa4f3f123fe92055fb3229b8c2b37
EvgeniyKuzin <vaglover@yandex.ru>
2023-12-23 22:07:39 +0300

    Initial Commit

---
+++ UIkitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitApp
+//
+//  Created by Evgeniy Kuzin on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
