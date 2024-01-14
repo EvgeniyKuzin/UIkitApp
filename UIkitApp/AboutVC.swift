37a9081632cf114272cfe0e47ff1e402e248e24f
EvgeniyKuzin <vaglover@yandex.ru>
2024-01-14 10:26:32 +0300

    add about VC and data transfer between VC

---
+++ UIkitApp/AboutVC.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  UIkitApp
+//
+//  Created by Evgeniy Kuzin on 13.01.2024.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+    
+    @IBOutlet var aboutLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        aboutLabel.text = "GGG"
+    }
+}
